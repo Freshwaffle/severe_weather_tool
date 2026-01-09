02cb9fdfcb2d0ea0ec1772e4bb21ffaae3ac1df3
Fresh <tlopez052609@gmail.com>
2026-01-08 20:43:06 -0500

    Revert To Previous State
    revert to 1

--- analysis/fronts.py
+++
@@ -1,8 +0,0 @@
-# analysis/fronts.py
-def dryline_proximity(lat, lon):
-    """
-    Placeholder function.
-    Returns approximate distance to nearest dryline/front in km.
-    """
-    # You could add real logic using shapefiles or SPC analyses later
-    return None
